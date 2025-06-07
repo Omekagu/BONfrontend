ca9e932603a86c4bb38f93811f7e8537b7033dae
Omekagu Chukwuebuka <mikecheq5@gmail.com>
2025-06-07 10:49:38 +0100

    Remove Android project files and configurations related to the Bon-hotel application, including keystore, ProGuard rules, AndroidManifest files, main activity and application classes, drawable resources, mipmap resources, color and string resources, styles, Gradle settings, and wrapper scripts. Update Babel configuration to include react-native-reanimated plugin.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,4 @@
-module.exports = function (api) {
-  api.cache(true)
-  return {
-    presets: ['babel-preset-expo']
-  }
+module.exports = {
+  presets: ['babel-preset-expo'], // or metro-react-native-babel-preset
+  plugins: ['react-native-reanimated/plugin'] // 👈 must be last
 }
