0ec41c01218ff7d5797f5ea8c0a7bab13bda6c9e
Omekagu Chukwuebuka <mikecheq5@gmail.com>
2025-06-07 11:44:32 +0100

    chore: Initialize Gradle wrapper and scripts for Android project
    - Added gradle-wrapper.properties to configure Gradle distribution.
    - Created gradlew and gradlew.bat scripts for executing Gradle commands.
    - Set default JVM options in both scripts.
    - Updated settings.gradle to include React Native and Expo plugins with autolinking support.
    - Configured metro.config.js to use Expo's default Metro configuration.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
-module.exports = {
-  presets: ['babel-preset-expo'], // or metro-react-native-babel-preset
-  plugins: ['react-native-reanimated/plugin'] // 👈 must be last
+module.exports = function (api) {
+  api.cache(true)
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: ['react-native-reanimated/plugin']
+  }
 }
